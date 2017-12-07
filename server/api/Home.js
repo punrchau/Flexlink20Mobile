77e877ee0dfcde67f77feda822a23107ded00214
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-07 15:48:37 +0530

    file added succefully[A

--- server/api/Home.js
+++ server/api/Home.js
@@ -5,7 +5,7 @@ exports.on_InvokeFlexLink = function (page) {
 }
 exports.on_InvokeOEM2 = function (page) {
     
-    page.action('Home','OEM2').screen('prehomeloadoem');
+    page.action('Home','OEM1').screen('prehomeloadoem');
 }
 exports.on_InvokePartMgmt = function (page) {
     //page.href('http://fl20tmobqa.cloudapp.net/#/partreceives').screen('prehomeloadjobmgmt');
