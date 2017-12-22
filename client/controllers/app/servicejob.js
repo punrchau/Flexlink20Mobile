22c51c5b5e98058ad4d3f87824d967cafaed0e60
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-22 01:09:11 -0800

    [Studio] Modified screen "ServiceJob"

--- client/controllers/app/servicejob.js
+++ client/controllers/app/servicejob.js
@@ -2,4 +2,10 @@ angular.module('app').controller('app_servicejob', app_servicejob);
 function app_servicejob($scope, app) {
     'use strict';
     app.init($scope);
+    $scope.expanded = false;
+    app.init($scope);
+
+    $scope.editRecord = function(index){
+        app.call('Home.editRecord',{'index':index});
+    }
 }
