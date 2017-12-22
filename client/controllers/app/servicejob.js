6d8eb8206d107bda3e5f242a488d0bcc8777d4fc
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-21 23:25:30 -0800

    [Studio] Modified screen "ServiceJob"

--- client/controllers/app/servicejob.js
+++ client/controllers/app/servicejob.js
@@ -1,5 +1,6 @@
 angular.module('app').controller('app_servicejob', app_servicejob);
 function app_servicejob($scope, app) {
     'use strict';
+     $scope.expanded = false;
     app.init($scope);
 }
