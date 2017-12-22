43b9a53e8a7dfe4aba5c8d24e1de533893485b25
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-22 12:12:56 +0530

    success 12:12

--- server/api/Home.js
+++ server/api/Home.js
@@ -13,7 +13,7 @@ exports.on_InvokePartMgmt = function (page) {
 }
 
 exports.on_InvokeServiceJob = function (page) {
-    page.href('http://fl20dev.azurewebsites.net/lenovo/#/repairjobs')
+    page.href('http://fl20dev.azurewebsites.net/lenovo/#/repairjobs').screen('servicejob');
 }
 
 
