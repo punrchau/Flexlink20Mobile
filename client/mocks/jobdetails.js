c7753e9316511f7bc65a0a8fd0ad2aeb174115b2
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-27 16:54:04 +0530

    27 10:23

--- client/mocks/jobdetails.js
+++ client/mocks/jobdetails.js
@@ -2,4 +2,5 @@
 window.mocks = window.mocks || {};
 window.mocks.jobdetails = window.mocks.jobdetails || {};
 window.mocks.jobdetails.actions_cancel = function ($scope, params, app) {
+    return app.go('app.servicejob');
 };
