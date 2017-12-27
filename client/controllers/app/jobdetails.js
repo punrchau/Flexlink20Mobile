6657819fa6587accc3c47de9f27ce2db69e81782
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-26 19:20:42 -0800

    [Studio] Modified screen "JobDetails"

--- client/controllers/app/jobdetails.js
+++ client/controllers/app/jobdetails.js
@@ -1,5 +1,13 @@
 angular.module('app').controller('app_jobdetails', app_jobdetails);
 function app_jobdetails($scope, app) {
     'use strict';
+    $scope.lst = [
+                    {categoryName:"Job Info", image:"images/jobdetails/info.png"},
+                    {categoryName:"Owner Info", image:"images/jobdetails/contact.png"},
+                    {categoryName:"Job Info", image:"images/jobdetails/info.png"},
+                    {categoryName:"Job Info", image:"images/jobdetails/info.png"},
+                    {categoryName:"Job Info", image:"images/jobdetails/info.png"},
+                ]
+                });
     app.init($scope);
 }
