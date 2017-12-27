8c12fe7ed6a7bff99db121669bb0bb97ca69225f
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-26 23:27:33 -0800

    [Studio] Update mock actions for "jobdetails"

--- client/mocks/jobdetails.js
+++ client/mocks/jobdetails.js
@@ -1,3 +1,5 @@
 'use strict';
 window.mocks = window.mocks || {};
 window.mocks.jobdetails = window.mocks.jobdetails || {};
+window.mocks.jobdetails.actions_cancel = function ($scope, params, app) {
+};
