65ef712e7ba8736f8f04b49c336530135ede9ad0
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-26 22:34:07 -0800

    [Studio] Modified screen "JobDetails"

--- client/controllers/app/jobdetails.js
+++ client/controllers/app/jobdetails.js
@@ -1,13 +1,13 @@
 angular.module('app').controller('app_jobdetails', app_jobdetails);
 function app_jobdetails($scope, app) {
     'use strict';
+    app.init($scope);
+    
     $scope.categories = [
                     {categoryName:"Job Info", image:"images/jobdetails/info.png"},
                     {categoryName:"Owner Info", image:"images/jobdetails/contact.png"},
                     {categoryName:"Job History", image:"images/jobdetails/History.png"}
                 ];
-               
-    app.init($scope);
     
     $scope.goToServiceJob = function () {
         if (window.mocks) {
