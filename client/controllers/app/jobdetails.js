747bad8f32adf704ea76cfc3dac4003705a4898e
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-27 02:02:40 -0800

    [Studio] Modified screen "JobDetails"

--- client/controllers/app/jobdetails.js
+++ client/controllers/app/jobdetails.js
@@ -3,14 +3,8 @@ function app_jobdetails($scope, app) {
     'use strict';
     
     app.init($scope,function(){
-         $scope.data.categories = [
-                    {categoryName:"Job Info1", image:"images/jobdetails/Service_Info.png"},
-                    {categoryName:"Owner Info", image:"images/jobdetails/Owner_Info.jpg"},
-                    {categoryName:"Job History", image:"images/jobdetails/History.jpg"},
-                    {categoryName:"Remarks", image:"images/jobdetails/Remarks.png"},
-                    {categoryName:"Photo", image:"images/jobdetails/Photo.jpg"},
-                    {categoryName:"Unit Info", image:"images/jobdetails/unit_info.png"}
-                ];  
+         $scope.data.categories = 
+                    {categoryName:"Job Info1", image:"images/jobdetails/Service_Info.png"}
     });
    
                 
