7fa9f1fa29a8f12da8f071c43da6026ac2aa58d4
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-27 10:38:19 +0530

    27 10:23

--- server/api/PartReceiveDetail.js
+++ server/api/PartReceiveDetail.js
@@ -1,3 +1,8 @@
 exports.goToHome = function(page, params) {
     page.action('PartReceiveDetails', 'actions.cancel');
 }
+
+exports.goToServiceJob = function(page, params) {
+    page.action('servicejob', 'actions.cancel');
+}
+
