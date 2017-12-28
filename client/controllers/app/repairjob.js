dc643af591069decd94e9f7c079cc3ab0b25e5f0
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-28 03:23:50 -0800

    [Studio] Modified screen "RepairJob"

--- client/controllers/app/repairjob.js
+++ client/controllers/app/repairjob.js
@@ -1,5 +1,6 @@
 angular.module('app').controller('app_repairjob', app_repairjob);
 function app_repairjob($scope, app) {
     'use strict';
+    $scope.chart.label='Ram Chart';
     app.init($scope);
 }
