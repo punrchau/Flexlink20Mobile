14313a3e2043bbb8b80f83e6c52da7c5ec22f351
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-28 01:20:09 -0800

    [Studio] Modified screen "ServiceJob"

--- client/controllers/app/servicejob.js
+++ client/controllers/app/servicejob.js
@@ -2,17 +2,5 @@ angular.module('app').controller('app_servicejob', app_servicejob);
 function app_servicejob($scope, app) {
     'use strict';
     $scope.expanded = false;
-    $scope.selectTabs=[
-    {
-      "$i": 0,
-      "tabName": "Repair Job",
-      "isSelected": true
-    },
-    {
-      "$i": 1,
-      "tabName": "Service Job",
-     
-    }
-  ];
     app.init($scope);
 }
