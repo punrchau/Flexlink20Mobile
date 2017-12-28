58a0ee1ceadb3a7440e720269c2d2490842464c9
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-27 20:28:21 -0800

    [Studio] Modified screen "JobDetails"

--- client/controllers/app/jobdetails.js
+++ client/controllers/app/jobdetails.js
@@ -1,6 +1,6 @@
 angular.module('app').controller('app_jobdetails', app_jobdetails);
 function app_jobdetails($scope, app) {
-    'use strict';
+   
     app.init($scope,function(){
         
         $scope.data.company ='Flex';
