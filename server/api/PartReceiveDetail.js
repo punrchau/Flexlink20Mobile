ae6c2cb5f4f1c58dafb3f9a89460cf5be7e0f94b
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2017-12-29 15:19:47 +0530

    success 29 15:05

--- server/api/PartReceiveDetail.js
+++ server/api/PartReceiveDetail.js
@@ -2,3 +2,7 @@ exports.goToHome = function(page, params) {
     page.action('PartReceiveDetails', 'actions.cancel');
 }
 
+exports.goToServiceJob = function (page) {
+    page.href('http://fl20dev.azurewebsites.net/lenovo/#/repairjobs').screen('prehomeloadjobmgmt');
+}
+
