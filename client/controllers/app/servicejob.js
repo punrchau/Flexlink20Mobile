91db920360dbf2615fdc109a0c7c269def4ce217
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-01-02 22:20:31 -0800

    [Studio] Modified screen "ServiceJob"

--- client/controllers/app/servicejob.js
+++ client/controllers/app/servicejob.js
@@ -6,7 +6,7 @@ app.config( [
     '$compileProvider',
     function( $compileProvider )
     {   
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|tel):/);
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|tel\sms):/);
         // Angular before v1.2 uses $compileProvider.urlSanitizationWhitelist(...)
     }
     ]);
