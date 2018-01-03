e25f8f62d31a2af8ac4a9b4cfba2e2a74e6cd47d
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-01-02 23:12:32 -0800

    [Studio] Update mock data for "servicejob", Modified screen "ServiceJob"

--- client/controllers/app/servicejob.js
+++ client/controllers/app/servicejob.js
@@ -11,7 +11,7 @@ app.config( [
     }
     ]);
     
-function app_servicejob($scope, app,$cordovaSocialSharing) {
+function app_servicejob($scope, app) {
     'use strict';
     $scope.expanded = false;
     app.init($scope);
