d2553f9a0e4d91cbbfa9d3454df0cb23537750c4
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-01-11 02:19:56 -0800

    [Studio] Modified screen "JobDetails"

--- client/controllers/app/jobdetails.js
+++ client/controllers/app/jobdetails.js
@@ -11,11 +11,11 @@ function app_jobdetails($scope, app) {
                 ]; */
                 
     $scope.jobinfos = {categoryName:'Job Info', image:'images/jobdetails/Service_Info.png'};
-    $scope.data.ownerinfo = {categoryName:'Owner Info', image:'images/jobdetails/Owner_Info.jpg'};
-    $scope.data.viewhistory = {categoryName:'Job History', image:'images/jobdetails/History.jpg'};
-    $scope.data.remarks = {categoryName:'Remarks', image:'images/jobdetails/Remarks.png'};
-    $scope.data.photo = {categoryName:'Photo', image:'images/jobdetails/Photo.jpg'};
-    $scope.data.unitinfo = {categoryName:'Unit Info', image:'images/jobdetails/unit_info.png'};
+    $scope.ownerinfo = {categoryName:'Owner Info', image:'images/jobdetails/Owner_Info.jpg'};
+    $scope.viewhistory = {categoryName:'Job History', image:'images/jobdetails/History.jpg'};
+    $scope.remarks = {categoryName:'Remarks', image:'images/jobdetails/Remarks.png'};
+    $scope.photo = {categoryName:'Photo', image:'images/jobdetails/Photo.jpg'};
+    $scope.unitinfo = {categoryName:'Unit Info', image:'images/jobdetails/unit_info.png'};
            
     app.init($scope);
     
