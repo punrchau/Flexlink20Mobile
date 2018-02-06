6e7e802688396ed87d250d0dd8c7c980d1bff7d8
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-02-05 21:55:51 -0800

    [Studio] Add screen "TestDetailsPage"

--- client/states.js
+++ client/states.js
@@ -177,5 +177,14 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
+    }).state('app.testdetailspage', {
+        views: {
+            app: {
+                controller: 'app_testdetailspage',
+                templateProvider: function (app) {
+                    return app.templateProvider('app.testdetailspage');
+                }
+            }
+        }
     });
 });
