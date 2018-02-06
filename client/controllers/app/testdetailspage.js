4f401c4b50ff4ba31df8d208556330f552abee55
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-02-05 22:07:38 -0800

    [Studio] Modified screen "TestDetailsPage"

--- client/controllers/app/testdetailspage.js
+++ client/controllers/app/testdetailspage.js
@@ -2,4 +2,5 @@ angular.module('app').controller('app_testdetailspage', app_testdetailspage);
 function app_testdetailspage($scope, app) {
     'use strict';
     app.init($scope);
+    $scope.date = new Date();
 }
