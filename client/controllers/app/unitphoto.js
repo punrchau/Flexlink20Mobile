9ec1a6832c8dd71a1103bc763593c42d09cf9d37
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-02-13 01:13:08 -0800

    [Studio] Modified screen "UnitPhoto"

--- client/controllers/app/unitphoto.js
+++ client/controllers/app/unitphoto.js
@@ -2,4 +2,11 @@ angular.module('app').controller('app_unitphoto', app_unitphoto);
 function app_unitphoto($scope, app) {
     'use strict';
     app.init($scope);
+    
+    $scope.uploadPhoto=function(){
+        app.action('unitphoto', 'upload', this);
+       app.action('unitphoto', 'cancel', this);
+    };
+    
+    
 }
