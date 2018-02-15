e63fe4c6393810fe23860b0ce89d37ca4f3bc0d9
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-02-15 14:43:38 +0530

    commit success 15 feb

--- server/api/Messages.js
+++ server/api/Messages.js
@@ -29,21 +29,21 @@ exports.on_messages = function (page) {
                 page.checkState(true);
             }
             
-            if (data.isphotoupload) {
-                page.extract('photoupload')
-                    .data(function (data) {
-                        page.screen('photoupload');
-                    });
-                page.checkState(true);
-            }
+            // if (data.isphotoupload) {
+            //     page.extract('photoupload')
+            //         .data(function (data) {
+            //             page.screen('servicejob');
+            //         });
+            //     page.checkState(true);
+            // }
 
-             if (data.isrepairjob) {
-                page.extract('servicejob')
-                    .data(function (data) {
-                        page.screen('servicejob');
-                    });
-                page.checkState(true);
-            }
+            //  if (data.isrepairjob) {
+            //     page.extract('servicejob')
+            //         .data(function (data) {
+            //             page.screen('servicejob');
+            //         });
+            //     page.checkState(true);
+            // }
 
             // if (data.isphotoupload) {
             //     page.extract('photoupload')
@@ -54,6 +54,16 @@ exports.on_messages = function (page) {
             //     page.extract('servicejob')
             //     page.checkState(true);
             // }
+
+             if (data.isphotoupload) {
+               page.extract('servicejob')
+                page.checkState(true);
+            }
+
+            if (data.isrepairjob) {
+                page.extract('servicejob')
+                page.checkState(true);
+            }
            
         });
 }
