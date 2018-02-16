f0a0375a31037bd10844eab155ae059e55541ef0
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-02-16 01:07:38 -0800

    [Studio] Modified screen "UnitInfo"

--- client/controllers/app/unitinfo.js
+++ client/controllers/app/unitinfo.js
@@ -1,7 +1,9 @@
 angular.module('app').controller('app_unitinfo', app_unitinfo);
 function app_unitinfo($scope,$rootScope, app) {
     'use strict';
-    app.init($scope);
+     app.init($scope, function () {
+     $scope.showPopup($scope.data);
+    });
     
     $scope.ticketnumber=$rootScope.ticketnumber;
     $scope.ownername=$rootScope.ownername;
