241c3a9251d886936a41cdf13644750b4622761d
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-02-20 00:49:47 -0800

    [Studio] Modified screen "PhotoUpload"

--- client/controllers/app/photoupload.js
+++ client/controllers/app/photoupload.js
@@ -11,11 +11,12 @@ function app_photoupload($scope, app) {
     };
    
    $scope.savePhoto = function () {
-       // $scope.base64string = angular.element(document.querySelector("#camera"))[0].value;
-       $scope.base64string = $scope.data.attachmentContent;
+        // $scope.base64string = angular.element(document.querySelector("#camera"))[0].value;
+        $scope.base64string = $scope.data.attachmentContent;
         $scope.descriptions = $scope.data.descriptions;
         app.call('Home.unitPhotoSave', {
-           'base64string': $scope.base64string
+           'base64string': $scope.base64string,
+           'descriptions': $scope.descriptions
         });
     }
 }
