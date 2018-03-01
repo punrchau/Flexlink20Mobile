d217cdad7a490868281604e6caf78cca06344fc5
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-02-28 21:18:33 -0800

    [Studio] Modified screen "JobHistoryDetails"

--- client/controllers/app/jobhistorydetails.js
+++ client/controllers/app/jobhistorydetails.js
@@ -5,4 +5,34 @@ function app_jobhistorydetails($scope,$rootScope, app) {
     
     $scope.ticketnumber=$rootScope.ticketnumber;
     $scope.ownername=$rootScope.ownername;
+    
+     $scope.goToRemarks=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.remarksPage', {});
+    };
+    
+    $scope.goToJobInfo=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.viewJobInfo', {});
+    };
+    
+     $scope.goToPhotoUpload=function(){
+       app.action('servicejobhistory', 'close', this);
+       app.call('Home.unitPhoto', {});
+    };
+    
+    $scope.goToUnitOwner=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.viewUnitOwnerInfo', {});
+    };
+    
+    $scope.goToHistory=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.viewJobHistory', {});
+    };
+      
+    $scope.goToUnitInfo=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.viewUnitInfo', {});
+    };
 }
