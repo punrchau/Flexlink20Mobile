f43dd8d5e9402a9a1aa09c8bce26f3812fc867fa
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-02-28 21:17:49 -0800

    [Studio] Modified screen "ServiceJobHistory"

--- client/controllers/app/servicejobhistory.js
+++ client/controllers/app/servicejobhistory.js
@@ -3,9 +3,38 @@ function app_servicejobhistory($scope,$rootScope, app) {
     'use strict';
     app.init($scope);
     
-     $scope.ticketnumber=$rootScope.ticketnumber;
+    $scope.ticketnumber=$rootScope.ticketnumber;
     $scope.ownername=$rootScope.ownername;
     
+    $scope.goToRemarks=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.remarksPage', {});
+    };
+    
+    $scope.goToJobInfo=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.viewJobInfo', {});
+    };
+    
+     $scope.goToPhotoUpload=function(){
+       app.action('servicejobhistory', 'close', this);
+       app.call('Home.unitPhoto', {});
+    };
+    
+    $scope.goToUnitOwner=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.viewUnitOwnerInfo', {});
+    };
+    
+    $scope.goToHistory=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.viewJobHistory', {});
+    };
+      
+    $scope.goToUnitInfo=function(){
+        app.action('servicejobhistory', 'close', this);
+        app.call('Home.viewUnitInfo', {});
+    };
     
     $scope.goToJobHistoryDetails = function (index) {
             //app.go('jobhistorydetails');
