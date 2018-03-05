4d7f53bc95ecbb8a3bfe74fba856f8504fec3c82
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-03-05 02:49:33 -0800

    [Studio] Modified screen "JobHistoryDetails"

--- client/controllers/app/jobhistorydetails.js
+++ client/controllers/app/jobhistorydetails.js
@@ -28,8 +28,7 @@ function app_jobhistorydetails($scope,$rootScope, app) {
     
     $scope.goToHistory=function(){
         app.action('servicejobhistory', 'close', this);
-        app.action('servicejob', 'actions.viewjobhistory', this);
-        //app.call('Home.viewJobHistory', {});
+        app.call('Home.viewJobHistory', {});
     };
       
     $scope.goToUnitInfo=function(){
