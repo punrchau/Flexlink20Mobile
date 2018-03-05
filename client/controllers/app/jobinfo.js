d68c85b738eb9c6d0264891d1cfe5a4782f67c9c
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-03-04 23:05:25 -0800

    [Studio] Modified screen "JobInfo"

--- client/controllers/app/jobinfo.js
+++ client/controllers/app/jobinfo.js
@@ -5,4 +5,29 @@ function app_jobinfo($scope,$rootScope, app) {
     
     $scope.ticketnumber=$rootScope.ticketnumber;
     $scope.ownername=$rootScope.ownername;
+    
+     $scope.goToHistory=function(){
+        app.action('jobinfo', 'close', this);
+        app.call('Home.viewJobHistory', {});
+    };
+    
+    $scope.goToPhotoUpload=function(){
+       app.action('jobinfo', 'close', this);
+       app.call('Home.unitPhoto', {});
+    };
+    
+    $scope.goToRemarks=function(){
+        app.action('jobinfo', 'close', this);
+        app.call('Home.remarksPage', {});
+    };
+    
+    $scope.goToUnitOwner=function(){
+        app.action('jobinfo', 'close', this);
+        app.call('Home.viewUnitOwnerInfo', {});
+    };
+    
+    $scope.goToUnitInfo=function(){
+        app.action('jobinfo', 'close', this);
+        app.call('Home.viewUnitInfo', {});
+    };
 }
