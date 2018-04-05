9f4b28b7ceb8be74bb03ec61425c769617951812
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-04-05 04:13:16 -0700

    [Studio] Modified screen "Flexlink20 Mobile"

--- client/controllers/app.js
+++ client/controllers/app.js
@@ -2,6 +2,19 @@ angular.module('app').controller('app', app);
 
 function app($scope, app, $ionicPopup) {
     'use strict';
+        if (window.isPlatform && window.isPlatform.powWow() && window.isPlatform.iOS() && window.cordova && window.cordova.plugins) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+        }
+        
+        if (window.StatusBar) {
+            StatusBar.hide();
+            document.body.classList.add('nostatusbar');
+        }
+
+        if (window.screen && window.screen.orientation) {
+            screen.orientation.lock('portrait');
+        }
+        
      app.loginModel = 'Login';
 
     $scope.showPopup = function (data) {
