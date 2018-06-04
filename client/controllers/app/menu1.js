c8560924fb9189e9acc20ad0547889094d76787a
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-06-04 03:31:48 -0700

    [Studio] Delete screen "app.menu1"

--- client/controllers/app/menu1.js
+++
@@ -1,5 +0,0 @@
-angular.module('app').controller('app_menu1', app_menu1);
-function app_menu1($scope, app) {
-    'use strict';
-    app.init($scope);
-}
--- client/states.js
+++ client/states.js
@@ -141,14 +141,5 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.menu1', {
-        views: {
-            app: {
-                controller: 'app_menu1',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.menu1');
-                }
-            }
-        }
     });
 });
