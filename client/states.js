9e48b0aa1d2c54ca0761fb75698e3fd5663937b4
Ramkewal Chauhan <ramkewal.chauhan@flex.com>
2018-06-04 03:30:05 -0700

    [Studio] Delete screen "app.uploadimage"

--- client/states.js
+++ client/states.js
@@ -33,15 +33,6 @@ angular.module('app').config(function ($stateProvider) {
                 }
             }
         }
-    }).state('app.uploadimage', {
-        views: {
-            app: {
-                controller: 'app_uploadimage',
-                templateProvider: function (app) {
-                    return app.templateProvider('app.uploadimage');
-                }
-            }
-        }
     }).state('app.prehomeloadflexlink', {
         views: {
             app: {
